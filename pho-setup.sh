de996659ffee3f9be7d4ad819628fc4f8d9782ac
diginfo <pac@dis.com.sg>
2020-05-29 13:38:57 +0800

    install

--- pho-setup.sh
+++ pho-setup.sh
@@ -16,7 +16,25 @@ BASHDIS="/etc/bashrc.dis";
 # -Djava.awt.headless=true = no-charts
 # -Djava.awt.headless=false = crash
 
+## Prompt Continue
+function _continue {
+  read -p "Continue Y/N? " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    exit 0
+  fi
+}
+
+## Prompt Wait ($1)
+function _wait {
+  echo
+  echo "$1"
+  _continue;
+}
+
+
 function rm_ {
+  wait_ "Remove & Purge Java & Pentaho ?"
   apt remove --purge -y libjfreechart-java default-jdk openjdk-8-jre openjdk-8-jre-headless;
   systemctl disable pure-pentaho.service;
   systemctl stop pure-pentaho;
@@ -27,6 +45,7 @@ function rm_ {
 
 ## Install Java
 function _java {
+  wait_ "Update O/S and Install ?
   apt-get -y update;
   apt install -y openjdk-8-jre libjfreechart-java;
   # mv /etc/java-8-openjdk/accessibility.properties /etc/java-8-openjdk/accessibility.properties.orig;
@@ -34,6 +53,7 @@ function _java {
 }
 
 function _alias {
+  wait_ "Install Aliases ?
   if [ ! -f "$BRC" ]; then
     echo ". $BRC" >> /etc/bash.bashrc
   fi
@@ -45,6 +65,7 @@ function _alias {
 }
 
 function _service {
+  wait_ "Install Service ?
   cp -rp $ROOT/pure-pentaho.service /etc/systemd/system
   chmod u+x /etc/systemd/system/pure-pentaho.service
   systemctl daemon-reload
@@ -54,6 +75,7 @@ function _service {
 }
 
 function _all {
+  wait_ "Full Install ?
   _rm;
   _java;
   _service;
