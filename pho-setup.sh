946e6dc14e8ae06c4075ca5d164ab8d55c96979e
diginfo <pac@dis.com.sg>
2020-05-29 13:35:37 +0800

    install

--- pho-setup.sh
+++ pho-setup.sh
@@ -17,7 +17,6 @@ BASHDIS="/etc/bashrc.dis";
 # -Djava.awt.headless=false = crash
 
 function rm_ {
-  mv /etc/java-8-openjdk/accessibility.properties.org /etc/java-8-openjdk/accessibility.properties;
   apt remove --purge -y libjfreechart-java default-jdk openjdk-8-jre openjdk-8-jre-headless;
   systemctl disable pure-pentaho.service;
   systemctl stop pure-pentaho;
@@ -27,23 +26,14 @@ function rm_ {
 }
 
 ## Install Java
-function java_ {
-  apt-get -y update
-  apt remove --purge -y default-jdk openjdk-8-jre openjdk-8-jre-headless
+function _java {
+  apt-get -y update;
   apt install -y openjdk-8-jre libjfreechart-java;
-  echo "@@@@ JAVA INSTALL DONE.";
   # mv /etc/java-8-openjdk/accessibility.properties /etc/java-8-openjdk/accessibility.properties.orig;
   # echo -e "## file moved to accessibility.properties.orig by pho-setup.sh\n\n" > /etc/java-8-openjdk/accessibility.properties
 }
 
-function pho_ {
-  cd $TGT
-  echo "Starting up Pure-Pentaho..."
-  $TGT/pho/pure-pentaho.sh start;
-  echo "@@@ PURE-PENTAHO INSTALLED.";
-}
-
-function alias_ {
+function _alias {
   if [ ! -f "$BRC" ]; then
     echo ". $BRC" >> /etc/bash.bashrc
   fi
@@ -54,7 +44,7 @@ function alias_ {
   source /etc/bash.bashrc;
 }
 
-function service_ {
+function _service {
   cp -rp $ROOT/pure-pentaho.service /etc/systemd/system
   chmod u+x /etc/systemd/system/pure-pentaho.service
   systemctl daemon-reload
@@ -63,15 +53,22 @@ function service_ {
   systemctl status pure-pentaho
 }
 
-if 
-[ $1 == "rm" ];then
-  echo "Removing JRE and Pentaho..."
-  rm_;
-  exit 0;
+function _all {
+  _rm;
+  _java;
+  _service;
+  _alias;  
+}
+
+function _help {
+  echo "USAGE pho-setup.sh | rm | all | java | service | alias | help"
+  echo "FULL INSTALL: pure3-install.sh all"    
+}
+
+if [ "$#" -eq 0 ]; then
+  _help;
 else
-  ## Execute
-  java_;
-  service_;
-  alias_;
-fi;
+  fn="_$1"
+  "$fn"
+fi
 
